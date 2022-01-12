adb94e27a10ad85b913cfe53758738343c241d4f
Lipeya <reljacer@naver.com>
2022-01-12 23:41:00 +0900

    crossorigin

--- src/main/java/com/hong/springapi/controller/StudyControllerlimpet.java
+++ src/main/java/com/hong/springapi/controller/StudyControllerlimpet.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins="*")
 @RequiredArgsConstructor
 @RestController
 public class StudyControllerlimpet {
