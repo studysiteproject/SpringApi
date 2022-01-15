01224399c7fc4e542ce55388714cf250a35f0e8e
lacram <sky114477@naver.com>
2022-01-15 23:01:14 +0900

    CORS method allow update

--- src/main/java/com/hong/springapi/filter/CorsFilter.java
+++ src/main/java/com/hong/springapi/filter/CorsFilter.java
@@ -25,6 +25,9 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
