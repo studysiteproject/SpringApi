6b9bd2a79a0468a46d911ef5d7f61e5b6d44c86c
Lipeya <reljacer@naver.com>
2022-01-26 00:06:32 +0900

    cors

---
+++ src/main/java/com/hong/springapi/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.hong.springapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/","https://catchstudys.com","https://www.catchstudys.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
